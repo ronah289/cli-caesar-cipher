39ecd76ef14549a94fc9255ffd3c9906185c8c3b
ronah289 <ronald.kibet@student.moringaschool.com>
2021-10-24 11:50:07 -0800

    implement getters and setters

--- src/main/java/Decoding.java
+++ src/main/java/Decoding.java
@@ -1,2 +1,27 @@
 public class Decoding {
+    private int decodingKey;
+    private String encodedText;
+
+    public int getDecodingKey() {
+        return decodingKey;
+    }
+
+    public int setDecodingKey(int decodingKey) {
+        this.decodingKey = decodingKey;
+        return decodingKey;
+    }
+
+    public String getEncodedText() {
+        return encodedText;
+    }
+
+    public String setEncodedText(String encodedText) {
+        this.encodedText = encodedText;
+        return encodedText;
+    }
+
+    public Decoding(int decodingValue, String textToDecode){
+        decodingKey = decodingValue;
+        encodedText = textToDecode;
+    }
 }
