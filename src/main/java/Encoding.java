c435f461a7b8550501ea99cfc57db5fbc91adda5
ronah289 <ronald.kibet@student.moringaschool.com>
2021-10-23 22:24:53 -0800

    implement getters and setters in encoding class

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Encoding {
+public class Encoding {
+    private int key;
+    private String input;
+
+    public Encoding(int newKey,String newInput){
+        key = newKey;
+        input = newInput;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public int setKey(int key) {
+        this.key = key;
+        return key;
+    }
+
+    public String getInput() {
+        String checkInput = "";
+        if(input.matches("[a-zA-Z]+")){
+            checkInput = input;
+        }
+        else {
+            checkInput = "check your string";
+        }
+        return checkInput;
+    }
+
+    public String setInput(String input) {
+        this.input = input;
+        return input;
+    }
 }
