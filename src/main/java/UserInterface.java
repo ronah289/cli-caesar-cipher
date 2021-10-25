ac9dd5f45c0c407a2352a772a6d94f8d0c4b77e8
ronah289 <ronald.kibet@student.moringaschool.com>
2021-10-24 16:27:05 -0800

    add result markers

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 public class UserInterface {
     public static void main(String[] args){
         Scanner inputs =new Scanner(System.in);
-        System.out.println("Caesar Cipher\n");
         System.out.println("Enter your message\n");
         String inputText = inputs.nextLine();
         System.out.println("Enter your key");
         int keyValue = inputs.nextInt();
         Encoding calculateEncoding = new Encoding(keyValue,inputText);
         Decoding calculateOutput = new Decoding(calculateEncoding.getKey(),calculateEncoding.getInput());
+        System.out.println("Caesar Cipher\n");
+        System.out.println("______________________");
         System.out.println("Input String: "+inputText.toUpperCase());
         System.out.println("Encrypted String: "+calculateEncoding.getInput());
         System.out.println("Decrypted String: "+calculateOutput.getEncodedText());
+        System.out.println("-------------------------------");
     }
 
 }
